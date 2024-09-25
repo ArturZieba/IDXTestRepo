c29c9a66f55536101f9e146e63aaa9f2892f573a
arturzieba97 <arturzieba97@gmail.com>
2024-09-25 08:05:32 +0000

    Add variable initialization via terminal input

--- RandomNumber.cpp
+++ RandomNumber.cpp
@@ -8,10 +8,14 @@ void randomIntegerInRange(int rangeMin, int rangeMax, int integersToGenerate)
     std::mt19937_64 randomNumberGenerator(std::time(nullptr));
     std::uniform_real_distribution<> distribution(rangeMin, rangeMax);
 
+    std::cout << integersToGenerate << " random integers in range " << rangeMin << " to " << rangeMax << ": \n\n";
+
     for (int i; i < integersToGenerate; i++)
     {
         std::cout << round(distribution(randomNumberGenerator)) << '\n';
     }
+
+    std::cout << '\n';
 }
 
 // TODO: Generate and print a specified number of random integers without repeating them
@@ -22,7 +26,22 @@ void randomUniqueIntegerInRange(int rangeMin, int rangeMax, int integersToGenera
 
 int main()
 {
+    int rangeMin = 5; //REMOVE INITIALIZATION AFTER FINISHING FUNCTIONS
+    int rangeMax = 10; //REMOVE INITIALIZATION AFTER FINISHING FUNCTIONS
+    int integersToGenerate = 5; //REMOVE INITIALIZATION AFTER FINISHING FUNCTIONS
+
+    //REMOVE COMMENT AFTER FINISHING FUNCTIONS
+    /*
+    std::cout << "Choose values for the random integer generation functions (please type in only integer values, max value higher than min, and non-negative number of numbers to generate)\n"; //TODO: Potentially add safeguards against unacceptable inputs?
+    std::cout << "Type in lower range: ";
+    std::cin >> rangeMin;
+    std::cout << "\nType in higher range: ";
+    std::cin >> rangeMax;
+    std::cout << "\nType in how many numbers to generate: ";
+    std::cin >> integersToGenerate;
+    */
+
     // TODO: Add accepting input from the Terminal
-    randomIntegerInRange(0, 20, 5);
-    randomUniqueIntegerInRange(14, 25, 36);
+    randomIntegerInRange(rangeMin, rangeMax, integersToGenerate);
+    randomUniqueIntegerInRange(rangeMin, rangeMax, integersToGenerate);
 }
