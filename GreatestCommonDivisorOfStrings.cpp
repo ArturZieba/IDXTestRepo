dd8447d1bffc4abe103275d6a85f63a38a89e6b0
arturzieba97 <arturzieba97@gmail.com>
2024-10-01 07:35:47 +0000

    Add a .cpp for the 1071. Greatest Common Divisor of Strings exercise

---
+++ GreatestCommonDivisorOfStrings.cpp
@@ -0,0 +1,16 @@
+//Source: https://leetcode.com/problems/greatest-common-divisor-of-strings/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+#include <string>
+
+std::string gcdOfStrings(std::string str1, std::string str2)
+{
+    std::string strsum = str1 + str2 + '\n';
+
+    return strsum;    
+}
+
+int main()
+{
+    std::cout << gcdOfStrings("Test", "tseT");
+}
