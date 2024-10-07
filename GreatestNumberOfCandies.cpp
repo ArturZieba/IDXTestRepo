411071fb77d68584c7ced44b50ed1c79b8743df4
arturzieba97 <arturzieba97@gmail.com>
2024-10-07 08:34:48 +0000

    Finish 1431. Kids With the Greatest Number of Candies exercise

--- GreatestNumberOfCandies.cpp
+++ GreatestNumberOfCandies.cpp
@@ -8,37 +8,73 @@ std::vector<bool> kidsWithCandies(std::vector<int>& candies, int extraCandies)
 {
     std::vector<bool> hasGreatestNumberOfCandies(candies.size());
 
+    int iterator = 0;
+
     for (int kid : candies)
     {
-        kid + extraCandies;
-        /*
+        kid += extraCandies;
+        
         if (kid >= *std::max_element(candies.begin(), candies.end())) 
         {
-            hasGreatestNumberOfCandies[kid] = true;
+            hasGreatestNumberOfCandies[iterator] = true;
+            //std::cout << "Iterator: " << iterator << " Value: " << kid << '\n';
         }
         else
         {
-            hasGreatestNumberOfCandies[kid] = false;
+            hasGreatestNumberOfCandies[iterator] = false;
+            //std::cout << "Iterator: " << iterator << " Value: " << kid << '\n';
         }
-        */
+
+        iterator++;
     }
+
+    //std::cout << '\n';
 
     return hasGreatestNumberOfCandies;
 }
 
+void printBoolVector(std::vector<bool> boolVector)
+{
+    std::cout << '['; 
+
+    for (int i; i < boolVector.size(); i++)
+    {  
+        if (boolVector[i] == 0)
+        {
+            std::cout << "false";
+        }
+        else
+        {
+            std::cout << "true";
+        }
+
+        if (i + 1 == boolVector.size())
+        {
+            std::cout << ']';
+        }
+        else
+        {
+            std::cout << ',';
+        }
+    }
+
+    std::cout << '\n';
+}
+
 int main()
 {
-    std::vector<int> candies0 = { 2, 3, 5, 1 ,3 };
+    std::vector<int> candies0 = { 2, 3, 5, 1 ,3 }; // Expected output: [true,true,true,false,true]
     int extraCandies0 = 3;
-    std::vector<int> candies1 = { 4, 2, 1, 1 ,2 };
+    std::vector<int> candies1 = { 4, 2, 1, 1 ,2 }; // Expected output: [true,false,false,false,false]
     int extraCandies1 = 1;
-    std::vector<int> candies2 = { 12, 1, 12 };
+    std::vector<int> candies2 = { 12, 1, 12 }; // Expected output: [true,false,true]
     int extraCandies2 = 10;
 
-    //std::vector<bool> greatestNumberOfCandies0 = kidsWithCandies(candies0, extraCandies0);
+    std::vector<bool> greatestNumberOfCandies0 = kidsWithCandies(candies0, extraCandies0);
+    std::vector<bool> greatestNumberOfCandies1 = kidsWithCandies(candies1, extraCandies1);
+    std::vector<bool> greatestNumberOfCandies2 = kidsWithCandies(candies2, extraCandies2);
 
-    for (bool mostCandies : kidsWithCandies(candies0, extraCandies0))
-    {
-        std::cout << mostCandies << '\n';
-    }
+    printBoolVector(greatestNumberOfCandies0);
+    printBoolVector(greatestNumberOfCandies1);
+    printBoolVector(greatestNumberOfCandies2);
 }
