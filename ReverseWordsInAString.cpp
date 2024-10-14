f68008d6d9fe0f573d00c4c0f3ad41bbb633340f
arturzieba97 <arturzieba97@gmail.com>
2024-10-14 08:12:09 +0000

    Add a .cpp for 151. Reverse Words in a String

---
+++ ReverseWordsInAString.cpp
@@ -0,0 +1,20 @@
+// Source: https://leetcode.com/problems/reverse-words-in-a-string/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+#include <string>
+
+std::string reverseWords(std::string s)
+{
+    return s;
+}
+
+int main()
+{
+    std::string s0 = "the sky is blue"; // Expected output: "blue is sky the"
+    std::string s1 = "  hello world  "; // Expected output: "world hello"
+    std::string s2 = "a good   example"; // Expected output: "example good a"
+
+    std::cout << reverseWords(s0) << '\n';
+    std::cout << reverseWords(s1) << '\n';
+    std::cout << reverseWords(s2) << '\n';
+}
