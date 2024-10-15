81b11681bfadde24db9aea105975619463ec906c
arturzieba97 <arturzieba97@gmail.com>
2024-10-15 08:48:41 +0000

    Continue work on 345. Reverse Vowels of a String

--- ReverseVowelsOfAString.cpp
+++ ReverseVowelsOfAString.cpp
@@ -1,10 +1,24 @@
 //Source: https://leetcode.com/problems/reverse-vowels-of-a-string/description/?envType=study-plan-v2&envId=leetcode-75
 
+#include <algorithm> // For std::find and std::search
+#include <cctype> // For std::tolower
 #include <iostream>
 #include <string>
+#include <vector>
 
 std::string reverseVowels (std::string s)
 {
+    // Vowels: 'a', 'e', 'i', 'o', 'u'
+    std::vector<char> vowels = { 'a', 'e', 'i', 'o', 'u' };
+
+    for(char& character : s)
+    {
+        if(std::find(vowels.begin(), vowels.end(), std::tolower(character)) != vowels.end()) // TODO: Check std::search
+        {
+            std::cout << character;
+        }
+    }
+
     return s;
 }
 
