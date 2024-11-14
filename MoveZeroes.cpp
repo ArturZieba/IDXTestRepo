ab10afb2cede132170aaf90e072864fca7a5dc6d
arturzieba97 <arturzieba97@gmail.com>
2024-11-14 08:20:22 +0000

    Finish 283. Move Zeroes

--- MoveZeroes.cpp
+++ MoveZeroes.cpp
@@ -1,5 +1,6 @@
 // Source: https://leetcode.com/problems/move-zeroes/description/?envType=study-plan-v2&envId=leetcode-75
 
+#include <algorithm> // For std::rotate
 #include <iostream>
 #include <vector>
 
@@ -7,15 +8,36 @@ void moveZeroes(std::vector<int>& nums)
 {
     for (int i = 0; i < nums.size(); i++)
     {
-        std::cout << nums[i];
+        if(nums[i] == 0)
+        {
+            std::rotate(nums.begin() + i, nums.begin() + i + 1, nums.end());
+        }
     }
+
+
+    // Print out the vector matching the expected output
+    std::cout << '[';
+
+    for (int i = 0; i < nums.size(); i++)
+    {
+        if (i == nums.size() - 1)
+        {
+            std::cout << nums[i];
+        }
+        else 
+        {
+            std::cout << nums[i] << ',';
+        }
+    }
+
+    std::cout << "]\n";
 }
 
 int main()
 {
-    std::vector<int> nums = { 0, 1, 0, 3, 12 }; // Expected output: [1,3,12,0,0]
+    std::vector<int> nums0 = { 0, 1, 0, 3, 12 }; // Expected output: [1,3,12,0,0]
+    std::vector<int> nums1 = { 0 }; // Expected output: [0]
 
-    moveZeroes(nums);
-
-    std::cout << "Test\n";
+    moveZeroes(nums0);
+    moveZeroes(nums1);
 }
