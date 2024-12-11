a61ee60ee63835b66c7d69fcdd3fac6e09f10575
arturzieba97 <arturzieba97@gmail.com>
2024-12-11 14:00:52 +0000

    Add a .cpp file for 1732. Find the Highest Altitude

---
+++ HighestAltitude.cpp
@@ -0,0 +1,18 @@
+// Source: https://leetcode.com/problems/find-the-highest-altitude/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+#include <vector>
+
+int largestAltitude(std::vector<int>& gain)
+{
+    return 1;
+}
+
+int main()
+{
+    std::vector gain0 = { -5, 1, 5, 0, -7 };
+    std::vector gain1 = { -4, -3, -2, -1, 4, 3, 2 };
+
+    std::cout << largestAltitude(gain0) << '\n';
+    std::cout << largestAltitude(gain1) << '\n';
+}
