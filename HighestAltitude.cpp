a5f80ec60df9cd44fff7d7aa84fa38e9b1c7aebc
arturzieba97 <arturzieba97@gmail.com>
2024-12-12 08:36:55 +0000

    Finish 1732. Find the Highest Altitude

--- HighestAltitude.cpp
+++ HighestAltitude.cpp
@@ -5,7 +5,20 @@
 
 int largestAltitude(std::vector<int>& gain)
 {
-    return 1;
+    int currentAltitude = 0;
+    int highestAltitude = currentAltitude;
+
+    for(int i = 0; i < gain.size(); i++)
+    {
+        currentAltitude += gain[i];
+
+        if(currentAltitude > highestAltitude)
+        {
+            highestAltitude = currentAltitude;  
+        }
+    }
+
+    return highestAltitude;
 }
 
 int main()
@@ -13,6 +26,6 @@ int main()
     std::vector gain0 = { -5, 1, 5, 0, -7 };
     std::vector gain1 = { -4, -3, -2, -1, 4, 3, 2 };
 
-    std::cout << largestAltitude(gain0) << '\n';
-    std::cout << largestAltitude(gain1) << '\n';
+    std::cout << largestAltitude(gain0) << '\n'; // Expected output: 1
+    std::cout << largestAltitude(gain1) << '\n'; // Expected output: 0
 }
