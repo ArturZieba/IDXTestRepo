8b1cdd68e2f8aef7b46bf596dc3ccd86c72674b1
arturzieba97 <arturzieba97@gmail.com>
2024-12-13 11:27:56 +0000

    Continue work on 643. Maximum Average Subarray I

--- MaxAverageSubarray.cpp
+++ MaxAverageSubarray.cpp
@@ -1,19 +1,23 @@
 // Source: https://leetcode.com/problems/maximum-average-subarray-i/description/?envType=study-plan-v2&envId=leetcode-75
 
+#include <algorithm>
 #include <iostream>
 #include <vector>
 
 double findMaxAverage(std::vector<int>& nums, int k)
 {
     int sum = 0;
+    std::vector<int> sumVector;
 
     for (int i = 0; i + k <= nums.size(); i++)
     {
         sum += nums[i];
-        // Get sums and compare them
+        sumVector.push_back(sum);
     }
 
-    return sum / k; // Average
+    int highestSum = *std::max_element(sumVector.begin(), sumVector.end());
+
+    return highestSum / k; // Average
 }
 
 int main()
