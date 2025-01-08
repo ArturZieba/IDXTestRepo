dc21b16dd2e1da9ef309e650293556b076fbc3cc
arturzieba97 <arturzieba97@gmail.com>
2025-01-08 09:04:25 +0000

    Add a .cpp for 2215. Find the Difference of Two Arrays

---
+++ LeetCode75/InProgress/DifferenceOfTwoArrays.cpp
@@ -0,0 +1,38 @@
+// Source: https://leetcode.com/problems/find-the-difference-of-two-arrays/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+#include <vector>
+
+std::vector<std::vector<int>> findDifference(std::vector<int>& nums1, std::vector<int>& nums2)
+{
+    return { nums1, nums2 };
+}
+
+
+int main()
+{
+    std::vector<int> nums01 = { 1, 2, 3 };
+    std::vector<int> nums02 = { 2, 4, 6 };
+    std::vector<int> nums11 = { 1, 2, 3, 3 };
+    std::vector<int> nums12 = { 1, 1, 2, 2 };
+
+    // Expected output: [[1,3],[4,6]]
+    for(auto element : findDifference(nums01, nums02))
+    {
+        for(int i; i < element.size(); i++)
+        {
+            std::cout << element[i];
+        }
+    }
+
+    std::cout << '\n';
+
+    // Expected output: [[3],[]]
+    for(auto element : findDifference(nums11, nums12))
+    {
+        for(int i; i < element.size(); i++)
+        {
+            std::cout << element[i];
+        }
+    }
+}
