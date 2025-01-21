f32799dd26c762e82f7d5d75583deeb562640205
arturzieba97 <arturzieba97@gmail.com>
2025-01-21 09:44:05 +0000

    Add a .cpp for 649. Dota2 Senate

---
+++ LeetCode75/InProgress/Dota2Senate.cpp
@@ -0,0 +1,18 @@
+// Source: https://leetcode.com/problems/dota2-senate/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+#include <string>
+
+std::string predictPartyVictory(std::string senate)
+{
+    return senate;
+}
+
+int main()
+{
+    std::string senate0 = "RD";
+    std::string senate1 = "RDD"; 
+
+    predictPartyVictory(senate0); // Expected output: "Radiant"
+    predictPartyVictory(senate1); // Expected output: "Dire"
+}
