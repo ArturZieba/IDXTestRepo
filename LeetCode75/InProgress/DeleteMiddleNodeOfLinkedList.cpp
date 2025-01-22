623900a8ed9d244e1b893d6d32b13e366652a09b
arturzieba97 <arturzieba97@gmail.com>
2025-01-22 09:19:37 +0000

    Add a .cpp for 2095. Delete the Middle Node of a Linked List

---
+++ LeetCode75/InProgress/DeleteMiddleNodeOfLinkedList.cpp
@@ -0,0 +1,33 @@
+// Source: https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+
+struct ListNode
+{
+    /*
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+    */
+};
+
+ListNode* deleteMiddle(ListNode* head)
+{
+    return head;
+}
+
+int main()
+{
+/*
+head0 = [1,3,4,7,1,2,6]
+// Expected output: [1,3,4,1,2,6]
+
+head1 = [1,2,3,4]
+// Expected output: [1,2,4]
+
+head2 = [2,1]
+// Expected output: [2]
+*/
+}
