246d5be33254f80c1e3f3101c20e86e5028f263d
arturzieba97 <arturzieba97@gmail.com>
2025-01-24 13:46:47 +0000

    Add a .cpp for 206. Reverse Linked List

---
+++ LeetCode75/InProgress/ReverseLinkedList.cpp
@@ -0,0 +1,25 @@
+// Source: https://leetcode.com/problems/reverse-linked-list/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int main()
+{
+/*
+Input: head0 = [1,2,3,4,5]
+// Expected Output: [5,4,3,2,1]
+
+Input: head1 = [1,2]
+// Expected Output: [2,1]
+
+Input: head2 = []
+// Expected Output: []
+*/
+}
