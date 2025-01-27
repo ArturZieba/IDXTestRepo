6e0d4b2a21139ad53e635e2d7119c8d5c4cf226a
arturzieba97 <arturzieba97@gmail.com>
2025-01-27 08:12:34 +0000

    Add a .cpp for 2130. Maximum Twin Sum of a Linked List

---
+++ LeetCode75/InProgress/MaximumTwinSumOfLinkedList.cpp
@@ -0,0 +1,30 @@
+// Source: https://leetcode.com/problems/maximum-twin-sum-of-a-linked-list/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+int pairSum(ListNode* head)
+{
+    return 123;
+}
+
+int main()
+{
+/*
+head0 = [5,4,2,1]
+// Expected Output: 6
+
+Input: head = [4,2,2,3]
+// Expected Output: 7
+
+Input: head = [1,100000]
+// Expected Output: 100001
+*/
+}
