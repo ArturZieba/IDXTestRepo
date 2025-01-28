4ed84ff19306d39992f28f397d4a89e339d5067a
arturzieba97 <arturzieba97@gmail.com>
2025-01-28 10:56:22 +0000

    Add a .cpp for 104. Maximum Depth of Binary Tree

---
+++ LeetCode75/InProgress/MaximumDepthOfBinaryTree.cpp
@@ -0,0 +1,29 @@
+// Source: https://leetcode.com/problems/maximum-depth-of-binary-tree/description/?envType=study-plan-v2&envId=leetcode-75
+
+#include <iostream>
+
+struct TreeNode 
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+int maxDepth(TreeNode* root) 
+{
+        return 321;
+}
+
+int main()
+{
+/*
+root0 = [3,9,20,null,null,15,7]
+// Expected Output: 3
+
+root1 = [1,null,2]
+// Expected Output: 2
+*/
+}
--- LeetCode75/InProgress/MaximumTwinSumOfLinkedList.cpp
+++ LeetCode75/InProgress/MaximumTwinSumOfLinkedList.cpp
@@ -2,7 +2,8 @@
 
 #include <iostream>
 
-struct ListNode {
+struct ListNode 
+{
     int val;
     ListNode *next;
     ListNode() : val(0), next(nullptr) {}
