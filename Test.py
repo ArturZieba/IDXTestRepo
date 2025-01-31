8fe9c5e3d6460c1619bea3d503337942a6f7c208
arturzieba97 <arturzieba97@gmail.com>
2025-01-31 11:45:07 +0000

    Check basic math operation in Test.py

---
+++ Test.py
@@ -0,0 +1,24 @@
+# import selenium
+
+print('Hello World 123')
+
+addition = 2 + 2
+print(addition)
+
+subtraction = 2 - 2
+print(subtraction)
+
+multiplication = 2 * 2
+print(multiplication)
+
+division = 2 / 2
+print(division)
+
+modulo = 5 % 3
+print(modulo)
+
+floorDivision = 3 // 2
+print(floorDivision)
+
+exponent = 3 ** 2
+print(exponent)
