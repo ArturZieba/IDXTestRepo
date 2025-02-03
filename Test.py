ba701019b00f0a5651d6ae00f82a10d5593f976e
arturzieba97 <arturzieba97@gmail.com>
2025-02-03 09:38:34 +0000

    Check basic operations on strings

--- Test.py
+++ Test.py
@@ -22,3 +22,46 @@ print(floorDivision)
 
 exponent = 3 ** 2
 print(exponent)
+
+my_number = 123
+print(type(my_number))
+
+my_string = 'Hello World'
+print(type(my_string))
+
+combination = 'a' + ' test ' + 'string'
+print(combination)
+
+double_quote = "I'm Bob"
+print(double_quote)
+
+escape_char = 'He\'s Bob ' + "and \"he's Bob\""
+print(escape_char)
+
+multiline_string = """All
+are
+Bob"""
+print(multiline_string)
+
+newline_string = 'Line 1\nLine 2'
+print(newline_string)
+
+carriage_string = 'carry\ron'
+print(carriage_string)
+
+tab_string = 'tab\tstring\\'
+print(tab_string)
+
+modified_string = 'Hello World\t 123'
+print(modified_string.lower())
+print(modified_string.upper())
+print(len(modified_string))
+print(modified_string.split(' '))
+print(modified_string.split())
+print(modified_string.replace('l', '*'))
+print(modified_string.replace('Hello', 'Bye'))
+print(modified_string[4] + modified_string[10])
+print(modified_string[::-1].upper())
+
+my_money = 20
+print(f'I have {my_money} money')
