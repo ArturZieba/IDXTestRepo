3fa0a55e84b4c03f4410fd47873dd30898899906
arturzieba97 <arturzieba97@gmail.com>
2025-03-14 09:13:43 +0000

    Check basic multiprocessing

--- Python/Test.py
+++ Python/Test.py
@@ -9,6 +9,7 @@ import traceback
 import os
 import shutil
 import subprocess
+import multiprocessing
 import threading
 import time
 
@@ -846,5 +847,53 @@ if __name__ == "__main__":
     threaded_other(80)
     end = time.time()
     print("Took: ", end - start)
+
+print('========\n')
+
+def heavy_multi(n, myid):
+    for x in range(1, n):
+        for y in range(1, n):
+            x**y
+        print(myid, "is done")
+
+def multiproc(n):
+    processes = []
+
+    for i in range(n):
+        p = multiprocessing.Process(target=heavy_multi, args=(500,i,))
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
+
+# if __name__ == "__main__":
+#     start = time.time()
+#     multiproc(80)
+#     end = time.time()
+#     print("Took: ", end - start)
+
+def heavy_multi_pool(n, myid):
+    for x in range(1, n):
+        for y in range(1, n):
+            x**y
+        print(myid, "is done")
+
+def doit(n):
+    heavy_multi_pool(500, n)
+
+def pooled(n):
+    with multiprocessing.Pool() as pool:
+        pool.map(doit, range(n))
+
+# if __name__ == "__main__":
+#     start = time.time()
+#     pooled(80)
+#     end = time.time()
+#     print("Took: ", end - start)
+
+print('========\n')
+
+
 
 print('========\n')
