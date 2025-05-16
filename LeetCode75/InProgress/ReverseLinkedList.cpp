3f1a133eaec83f47acbe2b678d56bd28b1d597ce
arturzieba97 <arturzieba97@gmail.com>
2025-05-16 08:41:41 +0000

    Continue work on 206. Reverse Linked List

--- LeetCode75/InProgress/ReverseLinkedList.cpp
+++ LeetCode75/InProgress/ReverseLinkedList.cpp
@@ -1,6 +1,7 @@
 // Source: https://leetcode.com/problems/reverse-linked-list/?envType=study-plan-v2&envId=leetcode-75
 
 #include <iostream>
+#include <vector>
 
 // Definition for singly-linked list.
 struct ListNode {
@@ -29,13 +30,46 @@ ListNode* reverseList(ListNode* head) {
 
 int main()
 {
-//ListNode* head0();
+    std::vector<int> head0Values = {1, 2, 3, 4, 5}; // head = [1,2,3,4,5]
+    ListNode* head0 = nullptr;
+    ListNode* current0 = nullptr;
+
+    for (int value : head0Values)
+    {
+        if (!head0)
+        {
+            head0 = new ListNode(value);
+            current0 = head0;
+        } 
+        else
+        {
+            current0 -> next = new ListNode(value);
+            current0 = current0 -> next;
+        } 
+    }
+
+    reverseList(head0);
+
+    std::cout << "[";
+
+    while (current0 != nullptr)
+    {
+        std::cout << current0 -> val;
+
+        if (current0 -> next != nullptr)
+        {
+            std::cout << ",";
+        }
+        current0 = current0 -> next;
+    }
+    
+    std::cout << "]";
 // Expected Output: [5,4,3,2,1]
 
-//ListNode* head1();
+//ListNode* head1(); // head = [1,2]
 // Expected Output: [2,1]
 
-//ListNode* head2();
+//ListNode* head2(); // head = []
 // Expected Output: []
 
 }
