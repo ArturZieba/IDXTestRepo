90d014add9e425c78bfdc0a5fb24db8dce3ae15d
arturzieba97 <arturzieba97@gmail.com>
2025-06-10 09:27:54 +0000

    Finish 1657. Determine if Two Strings Are Close

--- LeetCode75/Completed/HashMapSet/DetermineIfTwoStringsClose.cpp
+++ LeetCode75/Completed/HashMapSet/DetermineIfTwoStringsClose.cpp
@@ -1,8 +1,10 @@
 // Source: https://leetcode.com/problems/determine-if-two-strings-are-close/description/?envType=study-plan-v2&envId=leetcode-75
 
+#include <algorithm>
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 void printBool (bool bInput)
 {
@@ -24,43 +26,40 @@ bool closeStrings(std::string word1, std::string word2)
     }
 
     // Check if there is the same number of the same characters
-    std::unordered_map<int, int> uniqueValuesCount0;
-    std::unordered_map<int, int> uniqueValuesCount1;
+    std::unordered_map<int, int> uniqueValues0;
+    std::unordered_map<int, int> uniqueValues1;
 
     for(int i = 0; i < word1.size(); i++)
     {
-            uniqueValuesCount0[word1[i] - 'a']++; // - 'a' converts the char into int
-            uniqueValuesCount1[word2[i] - 'a']++;
+            uniqueValues0[word1[i] - 'a']++; // - 'a' converts the char into int
+            uniqueValues1[word2[i] - 'a']++;
     }
     
+    std::vector<int> uniqueValuesCount0;
+    std::vector<int> uniqueValuesCount1;
+
     for(int i = 0; i < 26; i++) // 26 for all lower case letters as per constraints
     {
         if(
-            ((uniqueValuesCount0.find(i) == uniqueValuesCount0.end())
-            && (uniqueValuesCount1.find(i) != uniqueValuesCount1.end())) 
+            ((uniqueValues0.find(i) == uniqueValues0.end())
+            && (uniqueValues1.find(i) != uniqueValues1.end())) 
             || 
-            ((uniqueValuesCount1.find(i) == uniqueValuesCount1.end())
-            && (uniqueValuesCount0.find(i) != uniqueValuesCount0.end()))
+            ((uniqueValues1.find(i) == uniqueValues1.end())
+            && (uniqueValues0.find(i) != uniqueValues0.end()))
         ) // Check if unordered_map contains the same set of unique characters (converted to int)
         {
             return false; // If it does not it cannot match the constraints
-        } /*else if (TBC)
+        } else if ((uniqueValues0.find(i) != uniqueValues0.end()) && (uniqueValues1.find(i) != uniqueValues1.end()))
         {
-           return true; 
-        }*/
+           uniqueValuesCount0.push_back(uniqueValues0[i]);
+           uniqueValuesCount1.push_back(uniqueValues1[i]);
+        }
     }
 
-    for(auto element : uniqueValuesCount0)
-    {
-        std::cout << element.first << " " << element.second << "\n";
-    }
+    std::sort(begin(uniqueValuesCount0), end(uniqueValuesCount0));
+    std::sort(begin(uniqueValuesCount1), end(uniqueValuesCount1));
 
-    for(auto element : uniqueValuesCount1)
-    {
-        std::cout << element.first << " " << element.second << "\n";
-    }
-
-    return 1;
+    return uniqueValuesCount0 == uniqueValuesCount1;
 }
 
 int main()
