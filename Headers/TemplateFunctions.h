acbcc7aa49cf082b640441d24bd395d233dd715d
arturzieba97 <arturzieba97@gmail.com>
2025-07-04 07:49:25 +0000

    Add a function for printing int vector elements in [] to TemplateFunctions.h

--- Headers/TemplateFunctions.h
+++ Headers/TemplateFunctions.h
@@ -2,6 +2,7 @@
 // Note that for readability sake this should not be included for most of the .cpp files in this repo, as they usually do not exceed ~60 lines of code
 // Include for bigger source files where most of listed functions would be used
 #include <iostream>
+#include <vector>
 
 // Function used to print true or false to the terminal depending on the passed boolean's value
 void printBool (bool bInput)
@@ -13,4 +14,21 @@ void printBool (bool bInput)
     {
         std::cout << "false\n";
     }
+}
+
+// Function used to print contents of int vector containers enclosed in [] (for example [1, 2, 3])
+void printVectorInt (std::vector<int> vInput)
+{
+    std::cout << '[';
+
+    for (int i = 0; i < vInput.size(); i++)
+    {
+        std::cout << vInput[i];
+        if (i != vInput.size() - 1)
+        {
+            std::cout << ',';
+        }
+    }
+
+    std::cout << "]\n";
 }
--- TemplateFunctions.cpp
+++ TemplateFunctions.cpp
@@ -5,4 +5,8 @@ int main()
     // Tests for functions from TemplateFunctions.h
     printBool(0); // Expected output: false
     printBool(1); // Expected output: true
+
+
+    std::vector<int> v = {1, 2, 3, 4, 5};
+    printVectorInt(v); // Expected output: [1,2,3,4,5]
 }
