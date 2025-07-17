c7d8a1d24a7d69689113bfee02334907b1d4e800
arturzieba97 <arturzieba97@gmail.com>
2025-07-17 07:42:55 +0000

    Continue to work on 735. Asteroid Collision

--- LeetCode75/InProgress/AsteroidCollision.cpp
+++ LeetCode75/InProgress/AsteroidCollision.cpp
@@ -26,59 +26,48 @@ std::vector<int> asteroidCollision(std::vector<int>& asteroids)
 
     for (int i = 0; i < asteroids.size(); i++)
     {
-        if (asteroids[i] > 0) // Asteroid is flying right
+        if (asteroid[i] > 0)
         {
-            // abs() to get absolute values to compare asteroid sizes
-            if (abs(asteroids[i]) > abs(asteroids [i + 1]) && asteroids [i + 1] < 0) // If asteroid is bigger than the one on the right, and the one on the right is flying left - destroy the asteroid on the right
-            {
-                indicesToErase.push_back(i + 1);
-            } 
-            else if (abs(asteroids[i]) == abs(asteroids[i + 1]) && asteroids [i + 1] < 0) // If asteroid is the same size as the one on the right, and the one on the right is flying left - destroy both
-            {
-                indicesToErase.push_back(i);
-                indicesToErase.push_back(i + 1);
-            }
-            else if (abs(asteroids[i]) < abs(asteroids [i + 1]) && asteroids [i + 1] < 0) // If asteroid is smaller than the one on the right, and the one on the right is flying left - destroy the current asteroid
-            {
-                indicesToErase.push_back(i);
-            }
-            else // If asteroid to the right is flying right
-            {
-                break;
-            }
-        } 
-        else if (asteroids[i] < 0) // Asteroid is flying right
+            indicesToErase.push_back(asteroid[i]);
+        }
+        else
         {
-            // abs() to get absolute values to compare asteroid sizes
-            if (abs(asteroids[i]) > abs(asteroids [i - 1]) && asteroids [i - 1] > 0) // If asteroid is bigger than the one on the left, and the one on the left is flying right - destroy the asteroid on the left
+            while (!indicesToErase.empty() && indicesToErase.back() > 0 && indicesToErase.back() < abs(asteroid[i]))
             {
-                indicesToErase.push_back(i - 1);
-            } 
-            else if (abs(asteroids[i]) == abs(asteroids[i - 1]) && asteroids [i - 1] > 0) // If asteroid is the same size as the one on the left, and the one on the left is flying right - destroy both
-            {
-                indicesToErase.push_back(i);
-                indicesToErase.push_back(i - 1);
+                indicesToErase.pop_back();
             }
-            else if (abs(asteroids[i]) < abs(asteroids [i - 1]) && asteroids [i - 1] > 0) // If asteroid is smaller than the one on the small, and the one on the left is flying right - destroy the current asteroid
+            if (!indicesToErase.empty() && indices.back() < 0)
             {
-                indicesToErase.push_back(i);
+                
             }
-            else // If asteroid to the right is flying left
-            {
-                break;
-            }
-        } 
+        }
 
-        printVectorInt(indicesToErase);
+        /*if (asteroids[i] > 0) // Asteroid is flying right
+        {
+            // abs() to get absolute values to compare asteroid sizes
+            if (abs(asteroids[i]) > abs(asteroids[i + 1]) && asteroids[i + 1] < 0) // If asteroid is bigger than the one on the right, and the one on the right is flying left - destroy the asteroid on the right
+            {
+                //indicesToErase.push_back(i + 1);
+                asteroids.erase(asteroids.begin() + (i + 1));
+                i--;
+            } 
+            else if (abs(asteroids[i]) == abs(asteroids[i + 1]) && asteroids[i + 1] < 0) // If asteroid is the same size as the one on the right, and the one on the right is flying left - destroy both
+            {
+                //indicesToErase.push_back(i);
+                //indicesToErase.push_back(i + 1);
+                asteroids.erase(asteroids.begin() + (i - 1));
+                asteroids.erase(asteroids.begin() + i, asteroids.begin() + (i + 1));
+                i--;
+                i--;
+            }
+            else if (abs(asteroids[i]) < abs(asteroids[i + 1]) && asteroids[i + 1] < 0) // If asteroid is smaller than the one on the right, and the one on the right is flying left - destroy the current asteroid
+            {
+                indicesToErase.push_back(i);
+            }
+        }*/ 
+
+        //printVectorInt(indicesToErase);
     }
-    
-    /*
-    for ()
-    {
-        asteroids.erase(asteroids.begin() + (i - 1));
-        asteroids.erase(asteroids.begin() + i, asteroids.begin() + (i + 1));
-    }
-    */
 
     return asteroids;
 } 
@@ -89,14 +78,16 @@ int main()
     std::vector<int> asteroids1 = { 8, -8 };
     std::vector<int> asteroids2 = { 10, 2, -5 };
 
+    /*
     std::cout << "0:\n";
     asteroidCollision(asteroids0);
     std::cout << "1:\n";
     asteroidCollision(asteroids1);
     std::cout << "2:\n";
     asteroidCollision(asteroids2);
+    */
 
-    //printVectorInt(asteroidCollision(asteroids0)); // Expected output: [5,10]
-    //printVectorInt(asteroidCollision(asteroids1)); // Expected output: []
-    //printVectorInt(asteroidCollision(asteroids2)); // Expected output: [10]
+    printVectorInt(asteroidCollision(asteroids0)); // Expected output: [5,10]
+    printVectorInt(asteroidCollision(asteroids1)); // Expected output: []
+    printVectorInt(asteroidCollision(asteroids2)); // Expected output: [10]
 }
