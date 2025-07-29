0aa49de2771ae9e59c85ba07169d4f64ae77dcb4
arturzieba97 <arturzieba97@gmail.com>
2025-07-29 07:38:28 +0000

    Continue work on 933. Number of Recent Calls

--- LeetCode75/InProgress/NumberOfRecentCalls.cpp
+++ LeetCode75/InProgress/NumberOfRecentCalls.cpp
@@ -1,28 +1,29 @@
 // Source: https://leetcode.com/problems/number-of-recent-calls/description/?envType=study-plan-v2&envId=leetcode-75
 
 #include <iostream>
+#include <vector>
 
 class RecentCounter
 {
 public:
     RecentCounter()
     {
-        
     }
-
-    int Counter = 0;
 
     int ping(int t)
     {
-        return t;
+        int Counter = 0;
+        std::vector<int> recentPings = {};
+        recentPings.push_back(t);
+
+        return recentPings.size();
     }
 };
 
 int main()
 {
     // Expected output: [null, 1, 2, 3, 3]
-    RecentCounter* recentCounter = new RecentCounter(); 
-    std::cout << recentCounter->Counter << '\n'; // Expected output: null
+    RecentCounter* recentCounter = new RecentCounter(); // Expected output: null
     std::cout << recentCounter->ping(1) << '\n'; // Expected output: 1
     std::cout << recentCounter->ping(100) << '\n'; // Expected output: 2
     std::cout << recentCounter->ping(3001) << '\n'; // Expected output: 3
