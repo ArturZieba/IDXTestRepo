0b6f2d3b2a28048731b1c158ed967489480e9e41
arturzieba97 <arturzieba97@gmail.com>
2025-07-31 07:44:49 +0000

    Continue work on 649. Dota2 Senate

--- LeetCode75/InProgress/Dota2Senate.cpp
+++ LeetCode75/InProgress/Dota2Senate.cpp
@@ -5,6 +5,29 @@
 
 std::string predictPartyVictory(std::string senate)
 {
+    while (!senate.find("R") || !senate.find("D"))
+    {
+        for (int i = 0; i < senate.size(); i++)
+        {
+            if(senate[i] - '0' == 'R' && !senate.find("D"))
+            {
+                return "Radiant";
+            }
+            else if(senate[i] - '0' == 'D' && !senate.find("R"))
+            {
+                return "Dire";
+            }
+            else if(senate[i] - '0' == 'R')
+            {
+                //senate.pop_back(senate.find("D"));
+            }
+            else if(senate[i] - '0' == 'D')
+            {
+                //senate.pop_back(senate.find("R"));
+            }
+        }
+    }
+
     return senate;
 }
 
@@ -13,6 +36,6 @@ int main()
     std::string senate0 = "RD";
     std::string senate1 = "RDD"; 
 
-    predictPartyVictory(senate0); // Expected output: "Radiant"
-    predictPartyVictory(senate1); // Expected output: "Dire"
+    std::cout << predictPartyVictory(senate0); // Expected output: "Radiant"
+    std::cout << predictPartyVictory(senate1); // Expected output: "Dire"
 }
