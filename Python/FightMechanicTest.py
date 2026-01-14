0065415018cb7cb3bd8b912d493321b32057e44c
arturzieba97 <arturzieba97@gmail.com>
2026-01-14 15:34:25 +0000

    Create a stump .py file for experimenting with simple RPG fight mechanic

---
+++ Python/FightMechanicTest.py
@@ -0,0 +1,14 @@
+class Player:
+    Health = 20
+    Damage = 3 # Make random
+
+class Enemy:
+    Health = 10
+    Damage = 2 # Make random
+
+# Spawn random objects (enemies)
+
+print(Player.Health)
+print(Player.Damage)
+print(Enemy.Health)
+print(Enemy.Damage)
