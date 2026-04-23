6c9533f7f2bca846e19375f6c0c35b73599258e4
arturzieba97 <arturzieba97@gmail.com>
2026-04-23 10:56:51 +0000

    Check different implementations of threads for the combat loop

--- Python/TestAsyncio.py
+++ Python/TestAsyncio.py
@@ -1,4 +1,5 @@
-import asyncio # For async defs/threading
+#import asyncio # For async defs/threading
+from threading import Thread # For Thread()
 
 #async def player_thread_function(player, enemy):
 #    while player.isalive == True: #player.isalive == True and enemy.isalive == True:
@@ -15,14 +16,24 @@ import asyncio # For async defs/threading
 #async def run_threads():
 #    await asyncio.gather(player_thread_function(playerinstance, enemyinstance), enemy_thread_function(enemyinstance, playerinstance))
 
-async def thread_test_one():
-    print("Test thread one")
+#def thread_test_one():
+#    for i in range(100):
+#        print("Test thread one")
 
-async def thread_test_two():
-    print("Test thread two")
+#def thread_test_two():
+#    for i in range(100):
+#        print("Test thread two")
 
-async def run_test_threads():
-    await asyncio.gather(thread_test_one(), thread_test_two()) # Make threads loop?
+#async def run_test_threads():
+#    await asyncio.gather(thread_test_one(), thread_test_two()) # Make threads loop?
+
+#asyncio.run(run_test_threads())
 
 print("Test")
-asyncio.run(run_test_threads())
+#thread1 = Thread(target = thread_test_one())
+#thread2 = Thread(target = thread_test_two())
+
+#thread1.start()
+#thread2.start()
+
+# Queue threads based on attack speed?
