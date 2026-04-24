f71c6db1e1ae5c5ed6cb08d18b8b98ea9967f906
arturzieba97 <arturzieba97@gmail.com>
2026-04-24 09:16:58 +0000

    Check setting up thread running within a while loop

--- Python/TestAsyncio.py
+++ Python/TestAsyncio.py
@@ -1,5 +1,6 @@
-#import asyncio # For async defs/threading
+import asyncio # For async defs/threading
 from threading import Thread # For Thread()
+import time # For time.sleep()
 
 #async def player_thread_function(player, enemy):
 #    while player.isalive == True: #player.isalive == True and enemy.isalive == True:
@@ -16,18 +17,20 @@ from threading import Thread # For Thread()
 #async def run_threads():
 #    await asyncio.gather(player_thread_function(playerinstance, enemyinstance), enemy_thread_function(enemyinstance, playerinstance))
 
-#def thread_test_one():
-#    for i in range(100):
-#        print("Test thread one")
+async def thread_test_one(attackspeed):
+    time.sleep(attackspeed)
+    print("Test thread one")
 
-#def thread_test_two():
-#    for i in range(100):
-#        print("Test thread two")
+async def thread_test_two(attackspeed):
+    time.sleep(attackspeed)
+    print("Test thread two")
 
-#async def run_test_threads():
-#    await asyncio.gather(thread_test_one(), thread_test_two()) # Make threads loop?
+async def run_test_threads():
+    attackspeedone = 2
+    attackspeedtwo = 1
+    await asyncio.gather(thread_test_one(attackspeedone), thread_test_two(attackspeedtwo)) # Make threads loop?
 
-#asyncio.run(run_test_threads())
+
 
 print("Test")
 #thread1 = Thread(target = thread_test_one())
@@ -37,3 +40,5 @@ print("Test")
 #thread2.start()
 
 # Queue threads based on attack speed?
+while True:
+    asyncio.run(run_test_threads())
