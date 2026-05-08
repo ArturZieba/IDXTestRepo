ab070af5175ea4881118fc2c50a06bccf417cb49
arturzieba97 <arturzieba97@gmail.com>
2026-05-08 10:26:15 +0000

    Test non-thread approach to player/enemy turns

--- Python/TestAsyncio.py
+++ Python/TestAsyncio.py
@@ -23,5 +23,32 @@ async def run_test_threads():
         print("Both deal and take damage at the same time")
         print(f"Equal: {attackspeedone}")
 
+def player_turn(attackspeed):
+    time.sleep(attackspeed)
+    print("Player attacks")
+    
+def enemy_turn(attackspeed):
+    time.sleep(attackspeed)
+    print("Enemy attacks")
+
+def run_turns():
+    attackspeedplayer = 1
+    attackspeedenemy = 1
+    if attackspeedplayer > attackspeedenemy:
+        player_turn(attackspeedplayer)
+        enemy_turn(attackspeedenemy)
+        print(f"Bigger: {attackspeedplayer}")
+    if attackspeedplayer < attackspeedenemy:
+        enemy_turn(attackspeedenemy)
+        player_turn(attackspeedplayer)
+        print(f"Lesser: {attackspeedplayer}")
+    if attackspeedplayer == attackspeedenemy:
+        time.sleep(attackspeedplayer)
+        print("Both deal and take damage at the same time")
+        print(f"Equal: {attackspeedplayer}")
+
 while True:
-    asyncio.run(run_test_threads())
+    #asyncio.run(run_test_threads())
+    run_turns()
+
+
