4f0715d2f7e4ae640b8a8b5bc7b8514e63b2bb9d
arturzieba97 <arturzieba97@gmail.com>
2026-05-11 10:01:04 +0000

    Check a different attack speed formula

--- Python/TestAsyncio.py
+++ Python/TestAsyncio.py
@@ -24,11 +24,11 @@ async def run_test_threads():
         print(f"Equal: {attackspeedone}")
 
 def player_turn(attackspeed):
-    time.sleep(attackspeed)
+    time.sleep(1/attackspeed)
     print("Player attacks")
     
 def enemy_turn(attackspeed):
-    time.sleep(attackspeed)
+    time.sleep(1/attackspeed)
     print("Enemy attacks")
 
 def run_turns():
@@ -43,7 +43,7 @@ def run_turns():
         player_turn(attackspeedplayer)
         print(f"Lesser: {attackspeedplayer}")
     if attackspeedplayer == attackspeedenemy:
-        time.sleep(attackspeedplayer)
+        time.sleep(1/attackspeedplayer) # 1/attackspeed -> 1/1 = 1 (1s sleep); 1/0.5 = 2 (2s sleep); 1/2 = 0.5 (0.5s sleep) - higher value == higher attack speed == shorter sleep
         print("Both deal and take damage at the same time")
         print(f"Equal: {attackspeedplayer}")
 
