92f5fab3d441d21204d880d2d4c9efd1a6b5ada4
arturzieba97 <arturzieba97@gmail.com>
2026-06-23 08:09:37 +0000

    Check input for keyboard input

--- Python/FightMechanicTest.py
+++ Python/FightMechanicTest.py
@@ -1,6 +1,4 @@
-#import keyboard # Does not work, needs sudo
 import math # For math.floor()
-from multiprocessing import Process # For Process()
 import random # For def randominteger
 import time # For time()
 
@@ -212,21 +210,16 @@ enemyinstance = Enemy()
 if __name__ == "__main__":
     playerinstance.info()
     enemyinstance.info()
-   
-    #Does not work, keyboard requires root access, maybe a different way of doing this?
-    #process = Process(target=run_turns(turnlength, playerinstance, enemyinstance))
-    #process.start()
-    #while process.is_alive():
-    #    if keyboard.is_pressed('q'):
-    #        process.terminate()
-    #        break
     
     # Run "gameplay loop" - player and enemy attacking based on their attack speed difference
+    input("Test: ")
+    
     run_turns(turnlength, playerinstance, enemyinstance)
 
     #Add gear/attributes/something to adjust player stats
     #Add comments to the async/turn code here and in the test file
-    #Don't start with gameloop, add a switch to choose game/exit/whatever
     #What to do when player dies (-exp?)
     #Add some rewards for levelup (increase hp?)
+
+    #Don't start with gameloop, add a switch to choose game/exit/whatever
     #Add console close without ctrl+c (workaround keyboard not being accessible without root)
--- Python/TestKeyboardInput.py
+++ Python/TestKeyboardInput.py
@@ -17,3 +17,5 @@ def test():
 #keyboard.type('Hello World')
 
 test()
+
+input("Test: ")
