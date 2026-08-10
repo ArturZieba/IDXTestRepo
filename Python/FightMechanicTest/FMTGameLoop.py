461eef5978eb99ede8c3ee788653afff9abaf728
arturzieba97 <arturzieba97@gmail.com>
2026-08-10 07:32:12 +0000

    Move gameloop() def to FMTGameLoop.py

--- Python/FightMechanicTest/FMTGameLoop.py
+++ Python/FightMechanicTest/FMTGameLoop.py
@@ -1,2 +1,136 @@
-def hello():
-    print("Hello from the other file")
+import time # For time()
+
+import FMTPlayer
+import FMTMain
+
+turnlength = 0.5 # Float variable used to adjust time between attack turns (lower means faster, higher means slower, see turn defs implementation for details)
+
+# Create initial Player and Enemy instances
+playerinstance = FMTPlayer.Player()
+enemyinstance = FMTMain.Enemy()
+
+# Turns when player's attackspeed is higher than the enemy's
+def player_turn(turnlength, player, enemy):
+    time.sleep(turnlength)
+    player.attack(enemy)
+    enemy.attack(player)
+    print(" ")
+
+# Turns when enemy's attackspeed is higher than the player's
+def enemy_turn(turnlength, player, enemy):
+    time.sleep(turnlength)
+    enemy.attack(player)
+    player.attack(enemy)
+    print(" ")
+
+# Turns when both the player and enemy attackspeed values are equal
+def both_turn(turnlength, player, enemy):
+    time.sleep(turnlength)
+    player.attack(enemy)
+    enemy.attack(player)
+    print(" ")
+
+# Run "fight loop" continuously - player and enemy attacking based on their attack speed difference - ends on player death
+def run_turns(turnlength, player, enemy):
+    while True:
+        while player.isalive & enemy.isalive:
+            # First check if player or enemy have more than 0 currenthealth
+            if enemy.currenthealth <= 0:
+                enemy.death(player)
+                # No return statement here, keeps going on until player's death in the return statement
+            if player.currenthealth <= 0:
+                player.death()
+                return # Exit loop when player dies
+
+            # If both the player and enemy remain alive then run turns based on their attackspeed
+            if player.attackspeed > enemy.attackspeed:
+                player_turn(turnlength, player, enemy)
+            if player.attackspeed < enemy.attackspeed:
+                enemy_turn(turnlength, player, enemy)
+            if player.attackspeed == enemy.attackspeed:
+                both_turn(turnlength, player, enemy)
+
+# Run "fight loop" once - player and enemy attacking based on their attack speed difference - ends on player or enemy death
+def run_turns_once(turnlength, player, enemy):
+    while True:
+        while player.isalive & enemy.isalive:
+            # First check if player or enemy have more than 0 currenthealth
+            if enemy.currenthealth <= 0:
+                enemy.death(player)
+                return # Exit loop when enemy dies
+            if player.currenthealth <= 0:
+                player.death()
+                return # Exit loop when player dies
+
+            # If both the player and enemy remain alive then run turns based on their attackspeed
+            if player.attackspeed > enemy.attackspeed:
+                player_turn(turnlength, player, enemy)
+            if player.attackspeed < enemy.attackspeed:
+                enemy_turn(turnlength, player, enemy)
+            if player.attackspeed == enemy.attackspeed:
+                both_turn(turnlength, player, enemy)
+
+# Main game loop
+def game_loop():
+    while True:
+        print("""1 - Automatic fight loop
+2 - Fight once
+3 - Player stats
+4 - Enemy stats
+5 - Exit
+
+DEBUG
+r - Revive player
+g - Set current and maximum player health
+n - Reroll current enemy
+t - Set turn length
+=====""")
+
+        # Input for the main menu selection
+        userinput = input("\nInput option chosen: ")
+        print(" ")
+
+        # 1 - Automatic fight loop
+        if userinput == "1":
+            run_turns(turnlength, playerinstance, enemyinstance)
+
+        # 2 - Fight once
+        elif userinput == "2":
+            run_turns_once(turnlength, playerinstance, enemyinstance)
+        
+        # 3 - Player stats
+        elif userinput == "3":
+            playerinstance.info()
+        
+        # 4 - Enemy stats
+        elif userinput == "4":
+            enemyinstance.info()
+        
+        # 5 - Exit
+        elif userinput == "5":
+            print("Exitting script")
+            break
+
+        # r - Revive player
+        elif userinput == "r":
+            playerinstance.currenthealth = playerinstance.maximumhealth
+            playerinstance.isalive = True
+            print("Revived player with current health set to maximum health\n")
+
+        # g - Set current and maximum player health
+        elif userinput == "g":
+            userinput = input("\nInput desired player maximum health: ")
+            print(" ")
+            playerinstance.maximumhealth = int(userinput) # Change string from input to integer to avoid issues with value comparison
+            playerinstance.currenthealth = playerinstance.maximumhealth
+
+        # n - Reroll current enemy
+        elif userinput == "n":
+            enemyinstance.spawnrandomenemy()
+
+        # t - Set turn length
+        elif userinput == "t":
+            changeturnlength()
+
+        else:
+            print(f"No {userinput} option, type in one of listed numbers")
--- Python/FightMechanicTest/FMTMain.py
+++ Python/FightMechanicTest/FMTMain.py
@@ -6,8 +6,6 @@ import FMTEnemyRoster # What does this conatin?
 import FMTGameLoop # What does this contain?
 import FMTPlayer # What does this contain?
 
-turnlength = 0.5 # Float variable used to adjust time between attack turns (lower means faster, higher means slower, see turn defs implementation for details)
-
 # Roll a random integer in min/max range
 def randominteger(min, max):
     generatedinteger = random.randint(min, max)
@@ -81,140 +79,15 @@ Experience granted: {self.experiencegranted}
         print(f"Experience granted: {self.experiencegranted}\n")
         self.spawnrandomenemy()
 
-# Turns when player's attackspeed is higher than the enemy's
-def player_turn(turnlength, player, enemy):
-    time.sleep(turnlength)
-    player.attack(enemy)
-    enemy.attack(player)
-    print(" ")
-
-# Turns when enemy's attackspeed is higher than the player's
-def enemy_turn(turnlength, player, enemy):
-    time.sleep(turnlength)
-    enemy.attack(player)
-    player.attack(enemy)
-    print(" ")
-
-# Turns when both the player and enemy attackspeed values are equal
-def both_turn(turnlength, player, enemy):
-    time.sleep(turnlength)
-    player.attack(enemy)
-    enemy.attack(player)
-    print(" ")
-
-# Run "fight loop" continuously - player and enemy attacking based on their attack speed difference - ends on player death
-def run_turns(turnlength, player, enemy):
-    while True:
-        while player.isalive & enemy.isalive:
-            # First check if player or enemy have more than 0 currenthealth
-            if enemy.currenthealth <= 0:
-                enemy.death(player)
-                # No return statement here, keeps going on until player's death in the return statement
-            if player.currenthealth <= 0:
-                player.death()
-                return # Exit loop when player dies
-
-            # If both the player and enemy remain alive then run turns based on their attackspeed
-            if player.attackspeed > enemy.attackspeed:
-                player_turn(turnlength, player, enemy)
-            if player.attackspeed < enemy.attackspeed:
-                enemy_turn(turnlength, player, enemy)
-            if player.attackspeed == enemy.attackspeed:
-                both_turn(turnlength, player, enemy)
-
-# Run "fight loop" once - player and enemy attacking based on their attack speed difference - ends on player or enemy death
-def run_turns_once(turnlength, player, enemy):
-    while True:
-        while player.isalive & enemy.isalive:
-            # First check if player or enemy have more than 0 currenthealth
-            if enemy.currenthealth <= 0:
-                enemy.death(player)
-                return # Exit loop when enemy dies
-            if player.currenthealth <= 0:
-                player.death()
-                return # Exit loop when player dies
-
-            # If both the player and enemy remain alive then run turns based on their attackspeed
-            if player.attackspeed > enemy.attackspeed:
-                player_turn(turnlength, player, enemy)
-            if player.attackspeed < enemy.attackspeed:
-                enemy_turn(turnlength, player, enemy)
-            if player.attackspeed == enemy.attackspeed:
-                both_turn(turnlength, player, enemy)
-
-# Main game loop
-def game_loop():
-    while True:
-        print("""1 - Automatic fight loop
-2 - Fight once
-3 - Player stats
-4 - Enemy stats
-5 - Exit
-
-DEBUG
-r - Revive player
-g - Set current and maximum player health
-n - Reroll current enemy
-t - Set turn length
-=====""")
-
-        # Input for the main menu selection
-        userinput = input("\nInput option chosen: ")
-        print(" ")
-
-        # 1 - Automatic fight loop
-        if userinput == "1":
-            run_turns(turnlength, playerinstance, enemyinstance)
-
-        # 2 - Fight once
-        elif userinput == "2":
-            run_turns_once(turnlength, playerinstance, enemyinstance)
-        
-        # 3 - Player stats
-        elif userinput == "3":
-            playerinstance.info()
-        
-        # 4 - Enemy stats
-        elif userinput == "4":
-            enemyinstance.info()
-        
-        # 5 - Exit
-        elif userinput == "5":
-            print("Exitting script")
-            break
-
-        # r - Revive player
-        elif userinput == "r":
-            playerinstance.currenthealth = playerinstance.maximumhealth
-            playerinstance.isalive = True
-            print("Revived player with current health set to maximum health\n")
-
-        # g - Set current and maximum player health
-        elif userinput == "g":
-            userinput = input("\nInput desired player maximum health: ")
-            print(" ")
-            playerinstance.maximumhealth = int(userinput) # Change string from input to integer to avoid issues with value comparison
-            playerinstance.currenthealth = playerinstance.maximumhealth
-
-        # n - Reroll current enemy
-        elif userinput == "n":
-            enemyinstance.spawnrandomenemy()
-
-        # t - Set turn length
-        elif userinput == "t":
-            changeturnlength()
-
-        else:
-            print(f"No {userinput} option, type in one of listed numbers") 
+ 
 
 if __name__ == "__main__":
-    # Create initial Player and Enemy instances
-    playerinstance = FMTPlayer.Player()
-    enemyinstance = Enemy()
+    
 
     # Start the full game loop
-    game_loop()
+    FMTGameLoop.game_loop()
 
+    #Check nad remove unused imports
     #Update import comments
     #Create new class for gameloop related defs (for example turns, turnlength change etc.)
     #Move existing gameloop related defs into a new class
