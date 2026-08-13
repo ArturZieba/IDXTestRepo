ae8ac0bf5d27eee68274e2bad7dd9fd2717542a0
arturzieba97 <arturzieba97@gmail.com>
2026-08-13 08:26:56 +0000

    Check and remove unused imports across all FMT files

--- Python/FightMechanicTest/FMTMain.py
+++ Python/FightMechanicTest/FMTMain.py
@@ -1,16 +1,9 @@
-import math # For math.floor()
-import random # For def randominteger()
-import time # For time()
-
-import FMTEnemyRoster # What does this conatin?
 import FMTGameLoop # What does this contain?
-import FMTPlayer # What does this contain?
 
 if __name__ == "__main__":
-        # Start the full game loop
+    # Start the full game loop
     FMTGameLoop.game_loop()
 
-    #Check nad remove unused imports
     #Check and Update all comments across all FMT files
     #Create new class for gameloop related defs (for example turns, turnlength change etc.)
     #Move existing gameloop related defs into a new class
