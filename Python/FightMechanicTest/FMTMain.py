008c09672f9503eae254132e0755d1f1c32fab8a
arturzieba97 <arturzieba97@gmail.com>
2026-08-18 09:28:18 +0000

    Check and update comments in FMTMain and FMTTools

--- Python/FightMechanicTest/FMTMain.py
+++ Python/FightMechanicTest/FMTMain.py
@@ -5,9 +5,6 @@ if __name__ == "__main__":
     FMTGameLoop.game_loop()
 
     #Check and update all comments
-    #Create new class for gameloop related defs (for example turns, turnlength change etc.)
-    #Move existing gameloop related defs into a new class
-    #Separate defs and classes into another file?
 
     #Add legitimate way to revive player
     #Add gear/attributes/something to adjust player stats
--- Python/FightMechanicTest/FMTTools.py
+++ Python/FightMechanicTest/FMTTools.py
@@ -1,5 +1,6 @@
 import random # For randint() def
 
+# Roll a random integer in the provided min - max range
 def randominteger(min, max):
     generatedinteger = random.randint(min, max)
     return generatedinteger
