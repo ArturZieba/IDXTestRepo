42372fcd8d7eb194870eee996c5aa219dd98ad09
arturzieba97 <arturzieba97@gmail.com>
2026-09-01 13:20:00 +0000

    Commit test

--- Python/FightMechanicTest/FMTMain.py
+++ Python/FightMechanicTest/FMTMain.py
@@ -4,8 +4,6 @@ if __name__ == "__main__":
     # Start the full game loop
     FMTGameLoop.game_loop()
 
-    #
-
     #Add legitimate way to revive player
     #Add option to revive player for free/gold?
     #Add a safeguard for starting the game if player is dead
