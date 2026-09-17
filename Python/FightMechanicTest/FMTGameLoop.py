f3fea9d9739bd8705c4644fe4600f804c1c8a371
arturzieba97 <arturzieba97@gmail.com>
2026-09-17 09:17:21 +0000

    Move menu printing to a separate mainmenu def

--- Python/FightMechanicTest/FMTGameLoop.py
+++ Python/FightMechanicTest/FMTGameLoop.py
@@ -24,6 +24,81 @@ def playerrevive():
     playerinstance.currenthealth = playerinstance.maximumhealth
     playerinstance.isalive = True
 
+def mainmenu():
+    while True:
+        print("""REGULAR MENU                           DEBUG MENU
+1 - Automatic fight loop                r - Revive player
+2 - Fight specified number of times     g - Set current and maximum player health
+3 - Fight once                          n - Reroll current enemy
+4 - Player stats                        t - Set turn length
+5 - Enemy stats
+6 - Exit
+=====""")
+
+        # Input for the main menu selection
+        userinput = input("\nInput option chosen: ")
+        print(" ")
+
+        # 1 - Automatic fight loop
+        if userinput == "1":
+            if playerinstance.isalive == True:
+                run_turns(turnlength, playerinstance, enemyinstance)
+            else:
+                fightstartplayerdeadprompt()
+
+        # 2 - Fight specified number of times
+        elif userinput == "2":
+            if playerinstance.isalive == True:
+                run_turns_multiple(turnlength, playerinstance, enemyinstance, 1)
+            else:
+                fightstartplayerdeadprompt()
+
+        # 3 - Fight once
+        elif userinput == "3":
+            if playerinstance.isalive == True:
+                run_turns_once(turnlength, playerinstance, enemyinstance)
+            else:
+                fightstartplayerdeadprompt()
+            
+        # 4 - Player stats
+        elif userinput == "4":
+            playerinstance.info()
+            
+        # 5 - Enemy stats
+        elif userinput == "5":
+            enemyinstance.info()
+            
+        # 6 - Exit
+        elif userinput == "6":
+            print("Exitting script")
+            break
+
+        # r - Revive player
+        elif userinput == "r":
+            playerrevive()
+            print("Revived player with current health set to maximum health\n")
+
+        # g - Set current and maximum player health
+        elif userinput == "g":
+            userinput = input("\nInput desired player maximum health: ")
+            print(" ")
+            playerinstance.maximumhealth = int(userinput) # Change string from input to integer to avoid issues with value comparison
+            playerinstance.currenthealth = playerinstance.maximumhealth
+            print(f"Maximum health set to {userinput}\n")
+
+        # n - Reroll current enemy
+        elif userinput == "n":
+            enemyinstance.spawnrandomenemy()
+            print(f"Enemy rerolled to {enemyinstance.name}\n")
+
+        # t - Set turn length
+        elif userinput == "t":
+            changeturnlength()
+
+        # Input not listed is provided
+        else:
+            print(f"No {userinput} option, type in one of listed numbers")
+
 def fightstartplayerdeadprompt():
     print("Player is dead\n")
     userinput = input("""1 - Revive for X gold with full Health
@@ -126,76 +201,4 @@ def run_turns_once(turnlength, player, enemy):
 
 # Main game loop
 def game_loop():
-    while True:
-        print("""REGULAR MENU                           DEBUG MENU
-1 - Automatic fight loop                r - Revive player
-2 - Fight specified number of times     g - Set current and maximum player health
-3 - Fight once                          n - Reroll current enemy
-4 - Player stats                        t - Set turn length
-5 - Enemy stats
-6 - Exit
-=====""")
-
-        # Input for the main menu selection
-        userinput = input("\nInput option chosen: ")
-        print(" ")
-
-        # 1 - Automatic fight loop
-        if userinput == "1":
-            if playerinstance.isalive == True:
-                run_turns(turnlength, playerinstance, enemyinstance)
-            else:
-                fightstartplayerdeadprompt()
-
-        # 2 - Fight specified number of times
-        elif userinput == "2":
-            if playerinstance.isalive == True:
-                run_turns_multiple(turnlength, playerinstance, enemyinstance, 1)
-            else:
-                fightstartplayerdeadprompt()
-
-        # 3 - Fight once
-        elif userinput == "3":
-            if playerinstance.isalive == True:
-                run_turns_once(turnlength, playerinstance, enemyinstance)
-            else:
-                fightstartplayerdeadprompt()
-        
-        # 4 - Player stats
-        elif userinput == "4":
-            playerinstance.info()
-        
-        # 5 - Enemy stats
-        elif userinput == "5":
-            enemyinstance.info()
-        
-        # 6 - Exit
-        elif userinput == "6":
-            print("Exitting script")
-            break
-
-        # r - Revive player
-        elif userinput == "r":
-            playerrevive()
-            print("Revived player with current health set to maximum health\n")
-
-        # g - Set current and maximum player health
-        elif userinput == "g":
-            userinput = input("\nInput desired player maximum health: ")
-            print(" ")
-            playerinstance.maximumhealth = int(userinput) # Change string from input to integer to avoid issues with value comparison
-            playerinstance.currenthealth = playerinstance.maximumhealth
-            print(f"Maximum health set to {userinput}\n")
-
-        # n - Reroll current enemy
-        elif userinput == "n":
-            enemyinstance.spawnrandomenemy()
-            print(f"Enemy rerolled to {enemyinstance.name}\n")
-
-        # t - Set turn length
-        elif userinput == "t":
-            changeturnlength()
-
-        # Input not listed is provided
-        else:
-            print(f"No {userinput} option, type in one of listed numbers")
+    mainmenu()
--- Python/FightMechanicTest/FMTMain.py
+++ Python/FightMechanicTest/FMTMain.py
@@ -4,7 +4,8 @@ if __name__ == "__main__":
     # Start the full game loop
     FMTGameLoop.game_loop()
 
-    #Menu print move to a separate def?
+    #Keep gameloop menu in a separate def or move back to gameloop? Separating would allow different options to be added
+    #Update comments
     #Move player dead prompts to a separate def
     #Game loop for certain number of kills? (i.e. stop after 5 kills or player death)
     #Further work on a legitimate way to revive player
